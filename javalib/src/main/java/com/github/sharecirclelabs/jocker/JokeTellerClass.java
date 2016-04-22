f8e650fe432ef15b08a4ce566342f3bad0dcd7a3
Denis Uraganov <github@uraganov.net>
2016-04-21 21:32:00 -0700

    JOK-9 - Make the button display a toast showing a joke retrieved from my Java joke telling library

--- javalib/src/main/java/com/github/sharecirclelabs/jocker/JokeTellerClass.java
+++ javalib/src/main/java/com/github/sharecirclelabs/jocker/JokeTellerClass.java
@@ -1,4 +1,7 @@
 package com.github.sharecirclelabs.jocker;
 
 public class JokeTellerClass {
+    public String getJoke(){
+        return "A clean house is the sign of a broken computer.";
+    }
 }
