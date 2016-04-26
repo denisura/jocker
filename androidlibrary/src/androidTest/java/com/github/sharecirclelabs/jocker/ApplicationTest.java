7a2e48329f6622a93200184919bf4175a1432545
Denis Uraganov <github@uraganov.net>
2016-04-25 22:33:37 -0700

    JOK-17 - Add Functional Tests

--- androidlibrary/src/androidTest/java/com/github/sharecirclelabs/jocker/ApplicationTest.java
+++ androidlibrary/src/androidTest/java/com/github/sharecirclelabs/jocker/ApplicationTest.java
@@ -2,6 +2,12 @@ package com.github.sharecirclelabs.jocker;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.text.TextUtils;
+
+import com.github.sharecirclelabs.jocker.task.EndpointsAsyncTask;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +15,38 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+
+    String mJoke = null;
+    Exception mError = null;
+    CountDownLatch signal = null;
+
+    @Override
+    protected void setUp() throws Exception {
+        signal = new CountDownLatch(1);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        signal.countDown();
+    }
+
+    public void testAsyncGetJokeTask() throws InterruptedException {
+        EndpointsAsyncTask task = new EndpointsAsyncTask();
+        task.setListener(new EndpointsAsyncTask.AsyncTaskListener() {
+            @Override
+            public void onComplete(String joke, Exception e) {
+                mJoke = joke;
+                mError = e;
+                signal.countDown();
+            }
+        }).execute();
+
+        //Set timeout for call to GCE module
+        signal.await(10, TimeUnit.SECONDS);
+
+        assertNull(mError);
+        assertFalse(TextUtils.isEmpty(mJoke));
     }
 }
