eee7b3a0ec896b4743b30cb76aae142d40e6bca6
Denis Uraganov <github@uraganov.net>
2016-04-27 20:36:43 -0700

    JOK-19 - Add Loading Indicator

--- androidlibrary/src/main/java/com/github/sharecirclelabs/jocker/JokeActivity.java
+++ androidlibrary/src/main/java/com/github/sharecirclelabs/jocker/JokeActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.github.sharecirclelabs.jocker.task.EndpointsAsyncTask;
 
 public class JokeActivity extends AppCompatActivity {
+    private ProgressBar spinner;
 
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, JokeActivity.class);
@@ -20,10 +23,14 @@ public class JokeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
+        spinner = (ProgressBar) findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
+
         EndpointsAsyncTask task = new EndpointsAsyncTask();
         task.setListener(new EndpointsAsyncTask.AsyncTaskListener() {
             @Override
             public void onComplete(String joke, Exception e) {
+                spinner.setVisibility(View.GONE);
                 TextView jokeTextView = (TextView) findViewById(R.id.joke_textview);
                 if (jokeTextView != null && joke != null && joke.length() != 0) {
                     jokeTextView.setText(joke);
